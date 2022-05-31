1444efa09f9d91e4e3932c6d4a3f1cad3d429e0c
neelxie <kidricederek@gmail.com>
2022-05-31 10:05:13 +0300

    create ico tokens

---
+++ hadhart/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xAB6A0d1CdB8d2d1733893C05C7CcC9FF3De746BE";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
